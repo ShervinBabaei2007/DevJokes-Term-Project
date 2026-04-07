3d1c4909f8b392eba8adda463b6c0ac21eede36d
Shervin Babaei <shervinb22@gmail.com>
2026-04-06 17:38:22 -0700

    feat: add capability to up/down vote only onces

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,7 @@ import type { CommentRow, JokeRow } from "#/server/db/schema";
 export type Joke = Pick<JokeRow, "id" | "question" | "answer" | "score"> & {
   comments: CommentRow["body"][];
   isOwner: boolean;
+  userVote: 1 | -1 | 0;
 };
 
 export interface CreateJokeInput {
